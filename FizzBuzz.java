8e7b484b120174a96e3723f97a5ca6d5007e5cbb
Fduranji <Franciscoduran93@gmail.com>
2016-08-25 13:52:51 -0600

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+/**
+   FizzBuzz class
+   Computer plays FizzBuzz up to
+   the number 100...
+*/
+
+public class FizzBuzz
+{
+   public static void main(String[] args)
+   {
+      for(int i = 1; i <= 100; i++)
+      {
+         String output = "";
+         if( i % 3 == 0) output += "Fizz";
+         if( i % 5 == 0) output += "Buzz";
+         if(output.equals("")) output += i;
+         
+         System.out.println(output);
+         
+         
+      }
+   }
+}
